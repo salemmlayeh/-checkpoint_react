1488ea8f6444701bb0f6d6295853f57202957287
salemmlayeh <salemmlayeh22@gmail.com>
2025-07-15 02:57:54 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Navbar, Card } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Salem Amine Mlayeh</Navbar.Brand>
+          </Container>
+        </Navbar>
+
+        <Container className="mt-4">
+          <h1>Welcome to My First Page</h1>
+
+          <div className="d-flex justify-content-between mt-4">
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>HTML</Card.Title>
+                <Card.Text>HyperText Markup Language.</Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>CSS</Card.Title>
+                <Card.Text>Cascading Style Sheets.</Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>JS</Card.Title>
+                <Card.Text>JavaScript.</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        </Container>
+      </div>
+    </>
   );
 }
 
